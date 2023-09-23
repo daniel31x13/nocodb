a2f9e6a546ec3920144dde40518faacf261e9c6a
sreehari jayaraj <sreeharivijaya2003@gmail.com>
2023-09-23 16:12:16 +0530

    fix: table ui base design and refactor type errros

--- packages/nc-gui/components/account/UserList.vue
+++ packages/nc-gui/components/account/UserList.vue
@@ -129,173 +129,49 @@ const copyPasswordResetUrl = async (user: User) => {
 
 <template>
   <div data-testid="nc-super-user-list">
-    <div class="max-w-[900px] mx-auto">
+    <div class="max-w-195 mx-auto">
       <div class="text-xl my-4 text-left font-weight-bold">User Management</div>
-      <div class="py-2 flex gap-4 items-center">
-        <a-input-search
+      <div class="py-2 flex gap-4 items-center justify-between">
+        <a-input
           v-model:value="searchText"
-          size="middle"
-          class="max-w-[300px]"
-          placeholder="Search Users"
-          @blur="loadUsers"
-          @keydown.enter="loadUsers"
+          class="!max-w-90 !rounded-md"
+          placeholder="Search members"
+          @blur="() => loadUsers"
+          @keydown.enter="() => loadUsers"
         >
-        </a-input-search>
-        <div class="flex-grow"></div>
-        <component :is="iconMap.reload" class="cursor-pointer" @click="loadUsers" />
-        <a-button
-          data-testid="nc-super-user-invite"
-          size="middle"
-          class="!rounded-md"
-          type="primary"
-          @click="
-            () => {
-              showUserModal = true
-              userMadalKey++
-            }
-          "
-        >
-          <div class="flex items-center gap-1">
-            <component :is="iconMap.plus" />
-            Invite new user
-          </div>
-        </a-button>
+          <template #prefix>
+            <PhMagnifyingGlassBold class="!h-3.5 text-gray-500" />
+          </template>
+        </a-input>
+        <div class="flex gap-3 items-center justify-center">
+          <component :is="iconMap.reload" class="cursor-pointer" @click="() => loadUsers" />
+          <NcButton
+            data-testid="nc-super-user-invite"
+            size="small"
+            type="primary"
+            @click="
+              () => {
+                showUserModal = true
+                userMadalKey++
+              }
+            "
+          >
+            <div class="flex items-center gap-1">
+              <component :is="iconMap.plus" />
+              Invite new user
+            </div>
+          </NcButton>
+        </div>
       </div>
-      <a-table
-        :row-key="(record) => record.id"
-        :data-source="users"
-        :pagination="pagination"
-        :loading="isLoading"
-        size="small"
-        @change="loadUsers($event.current)"
-      >
-        <template #emptyText>
-          <a-empty :image="Empty.PRESENTED_IMAGE_SIMPLE" :description="$t('labels.noData')" />
-        </template>
-
-        <!-- Email -->
-        <a-table-column key="email" :title="$t('labels.email')" data-index="email">
-          <template #default="{ text }">
-            <div>
-              {{ text }}
-            </div>
-          </template>
-        </a-table-column>
-
-        <!-- Role -->
-        <a-table-column key="roles" :title="$t('objects.role')" data-index="roles">
-          <template #default="{ record }">
-            <div>
-              <div v-if="record.roles.includes('super')" class="font-weight-bold">Super Admin</div>
-              <a-select
-                v-else
-                v-model:value="record.roles"
-                class="w-[220px] nc-user-roles"
-                :dropdown-match-select-width="false"
-                @change="updateRole(record.id, record.roles)"
-              >
-                <a-select-option
-                  class="nc-users-list-role-option"
-                  :value="OrgUserRoles.CREATOR"
-                  :label="$t(`objects.roleType.orgLevelCreator`)"
-                >
-                  <div>{{ $t(`objects.roleType.orgLevelCreator`) }}</div>
-                  <span class="text-gray-500 text-xs whitespace-normal">
-                    {{ $t('msg.info.roles.orgCreator') }}
-                  </span>
-                </a-select-option>
-
-                <a-select-option
-                  class="nc-users-list-role-option"
-                  :value="OrgUserRoles.VIEWER"
-                  :label="$t(`objects.roleType.orgLevelViewer`)"
-                >
-                  <div>{{ $t(`objects.roleType.orgLevelViewer`) }}</div>
-                  <span class="text-gray-500 text-xs whitespace-normal">
-                    {{ $t('msg.info.roles.orgViewer') }}
-                  </span>
-                </a-select-option>
-              </a-select>
-            </div>
-          </template>
-        </a-table-column>
-
-        <!--        &lt;!&ndash; Projects &ndash;&gt;
-        <a-table-column key="projectsCount" :title="$t('objects.projects')" data-index="projectsCount">
-          <template #default="{ text }">
-            <div>
-              {{ text }}
-            </div>
-          </template>
-        </a-table-column> -->
-
-        <!-- Actions -->
-
-        <a-table-column key="id" :title="$t('labels.actions')" data-index="id">
-          <template #default="{ text, record }">
-            <div v-if="!record.roles.includes('super')" class="flex items-center gap-2">
-              <a-dropdown :trigger="['click']" class="flex" placement="bottomRight" overlay-class-name="nc-dropdown-user-mgmt">
-                <div class="flex flex-row items-center">
-                  <a-button type="text" class="!px-0">
-                    <div class="flex flex-row items-center h-[1.2rem]">
-                      <component :is="iconMap.threeDotHorizontal" class="nc-user-row-action" />
-                    </div>
-                  </a-button>
-                </div>
-
-                <template #overlay>
-                  <a-menu>
-                    <template v-if="record.invite_token">
-                      <a-menu-item>
-                        <!-- Resend invite Email -->
-                        <div class="flex flex-row items-center py-3" @click="resendInvite(record)">
-                          <component :is="iconMap.email" class="flex h-[1rem] text-gray-500" />
-                          <div class="text-xs pl-2">{{ $t('activity.resendInvite') }}</div>
-                        </div>
-                      </a-menu-item>
-                      <a-menu-item>
-                        <div class="flex flex-row items-center py-3" @click="copyInviteUrl(record)">
-                          <component :is="iconMap.copy" class="flex h-[1rem] text-gray-500" />
-                          <div class="text-xs pl-2">{{ $t('activity.copyInviteURL') }}</div>
-                        </div>
-                      </a-menu-item>
-                    </template>
-                    <a-menu-item>
-                      <div class="flex flex-row items-center py-3" @click="copyPasswordResetUrl(record)">
-                        <component :is="iconMap.copy" class="flex h-[1rem] text-gray-500" />
-                        <div class="text-xs pl-2">{{ $t('activity.copyPasswordResetURL') }}</div>
-                      </div>
-                    </a-menu-item>
-                    <a-menu-item>
-                      <div class="flex flex-row items-center py-3" @click="isOpen = true">
-                        <!-- Delete user modal -->
-                        <GeneralDeleteModal v-model:visible="isOpen" entity-name="User" :on-delete="() => deleteUser(text)">
-                          <template #entity-preview>
-                            <span>
-                              <div class="flex flex-row items-center py-2.25 px-2.5 bg-gray-50 rounded-lg text-gray-700 mb-4">
-                                <GeneralIcon icon="account" class="nc-view-icon"></GeneralIcon>
-                                <div
-                                  class="capitalize text-ellipsis overflow-hidden select-none w-full pl-1.75"
-                                  :style="{ wordBreak: 'keep-all', whiteSpace: 'nowrap', display: 'inline' }"
-                                >
-                                  {{ record.email }}
-                                </div>
-                              </div>
-                            </span>
-                          </template>
-                        </GeneralDeleteModal>
-                        <component :is="iconMap.delete" data-testid="nc-super-user-delete" class="flex h-[1rem] text-gray-500" />
-                        <div class="text-xs pl-2">{{ $t('general.delete') }}</div>
-                      </div>
-                    </a-menu-item>
-                  </a-menu>
-                </template>
-              </a-dropdown>
-            </div>
-            <span v-else></span>
-          </template>
-        </a-table-column>
-      </a-table>
+      <div class="w-[780px] mt-5 border-1 rounded-md h-[520px]">
+        <div>
+          <div class="flex w-full pl-5 bg-gray-50 border-b-1">
+            <span class="py-3.5 text-gray-500 font-medium text-3.5 w-1/4 text-start">{{ $t('labels.email') }}</span>
+            <span class="py-3.5 text-gray-500 font-medium text-3.5 w-2/4 text-start">{{ $t('objects.role') }}</span>
+            <span class="py-3.5 pl-19 text-gray-500 font-medium text-1/4 w-2/9 text-start">Actions</span>
+          </div>
+        </div>
+      </div>
 
       <LazyAccountUsersModal :key="userMadalKey" :show="showUserModal" @closed="showUserModal = false" @reload="loadUsers" />
     </div>
